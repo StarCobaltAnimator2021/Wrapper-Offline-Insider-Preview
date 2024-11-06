881120f92625a92c70e8a029988dea81fc41d0b6
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2024-11-06 16:11:03 +0700

    goodbye oct

--- Start.bat
+++ Start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo:
 echo Wrapper Offline Insider Preview
-echo Evaluation copy. Build 21296.rs_prerelease.241001-0657
+echo Evaluation copy. Build 21301.rs_prerelease.241101-0850
 echo:
 npm start
--- server/pages/checkver.bat
+++ server/pages/checkver.bat
@@ -1,13 +1,11 @@
 @Echo off
 title Wrapper Offline Version Checker
 echo Wrapper Offline Insider Preview
-echo Version 2.0.0 (Build 21296.rs_prerelease.241001-0657)
+echo Version 2.0.0 (Build 21301.rs_prerelease.241101-0850)
 echo:
 echo New Features and Changes
 echo:
-echo 1. The footer has been reduced from 16px to 13px
-echo:
-echo 2. The theme has been changed into Halloween to reflect the Halloween Month.
+echo The UI were reverted to default due to Halloween is over.
 echo:
 pause
 cls
