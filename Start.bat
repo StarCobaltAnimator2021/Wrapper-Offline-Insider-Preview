7aa0e690fdd5ce8def6145326c4ce451f781c6e6
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2024-12-11 17:29:20 +0700

    brrrr

--- Start.bat
+++ Start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo:
 echo Wrapper Offline Insider Preview
-echo Evaluation copy. Build 21313.rs_prerelease.241126-1416
+echo Evaluation copy. Build 21318.rs_prerelease.241211-1710
 echo:
 npm start
--- server/pages/checkver.bat
+++ server/pages/checkver.bat
@@ -1,15 +1,13 @@
 @Echo off
 title Wrapper Offline Version Checker
 echo Wrapper Offline Insider Preview
-echo Version 2.0.1 (Build 21313.rs_prerelease.241126-1416)
+echo Version 2.0.1 (Build 21318.rs_prerelease.241211-1710)
 echo:
 echo New Features and Changes
 echo:
-echo 1. Version has been updated from 2.0.0 to 2.0.1
+echo 1. The Mermaid & New Hairstyle Assets, first introduced in GAWN2600 build 22557.1, then removed in build 22579.1, and then re-added back in build 22593.1, has been backported to this build.
 echo:
-echo 2. The revamped Install.bat as Preinstallation Environment, first introduced in GAWN2600 build 22572.1, has been backported to this build.
-echo:
-echo 3. The UI has been changed into Christmas Theme due to December is near.
+echo 2. The Exclusive Poses, first introduced in GAWN2600 build 22538.1000, has been backported to this build.
 echo:
 pause
 cls
