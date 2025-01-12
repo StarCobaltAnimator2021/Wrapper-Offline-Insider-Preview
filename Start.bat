77aaebdbbfa4a58c6c45cdb412a53b4f8f9d3459
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2025-01-12 17:42:09 +0700

    no new features

--- Start.bat
+++ Start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo:
 echo Wrapper Offline Insider Preview
-echo Evaluation copy. Build 21318.rs_prerelease.241211-1710
+echo Evaluation copy. Build 21322.rs_prerelease.241220-1840
 echo:
 npm start
--- server/pages/checkver.bat
+++ server/pages/checkver.bat
@@ -1,13 +1,11 @@
 @Echo off
 title Wrapper Offline Version Checker
 echo Wrapper Offline Insider Preview
-echo Version 2.0.1 (Build 21318.rs_prerelease.241211-1710)
+echo Version 2.0.1 (Build 21322.rs_prerelease.241220-1840)
 echo:
 echo New Features and Changes
 echo:
-echo 1. The Mermaid & New Hairstyle Assets, first introduced in GAWN2600 build 22557.1, then removed in build 22579.1, and then re-added back in build 22593.1, has been backported to this build.
-echo:
-echo 2. The Exclusive Poses, first introduced in GAWN2600 build 22538.1000, has been backported to this build.
+echo No New Features were included in this build.
 echo:
 pause
 cls
